f32d2898ef7caba2758ece81d71d9bb0dd0913ab
unknown <Administrator@Newcaed61.MDServer.bvb.edu>
2024-03-18 10:48:03 +0530

    code for Calculator

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+    int num1=10;
+    int num2=5;
+    
+
+    public int Addition(int num1,int num2){
+        int add=num1+num2;
+        return add;
+    }
+    public int Substraction(int num1,int num2){
+        int sub=num1-num2;
+        return sub;
+    }
+    public int Multiplication(int num1,int num2){
+        int mul=num1*num2;
+        return mul;
+    }
+}
